807e97c20edac7260cf4b7093c91fe4569d73559
Pedro Araujo <araujopedrop@gmail.com>
2025-03-07 18:37:51 -0300

    Test with snapshots

--- 03-counter-app-vite/tests/FirstApp.test.jsx
+++ 03-counter-app-vite/tests/FirstApp.test.jsx
@@ -5,7 +5,29 @@ describe('Pruebas en <FirstApp />', () => {
     test('Debe de hace match con el snapshot', () => { 
 
         const title = 'Hola, soy Goku';
-        // Renderizar el componente en memoria
-        render( <FirstApp title={ title } />);
+        // Renderizar el componente en memoria 
+        // Actualiza el objeto Screen (Propio de react testing library)
+        const { container } = render( <FirstApp title={ title } />);
+
+        console.log(container);
+        expect( container ).toMatchSnapshot();
+    });
+
+    test('Debe de mostrar el titulo en un h1', () => { 
+
+        const title = 'Hola, soy Goku';
+        const { container, getByText } = render( <FirstApp title={ title } />);   
+
+        // Espero que en el component renderizado es el texto que busco
+        expect (getByText(title)).toBeTruthy();
+
+        const h1 = container.querySelector('h1');
+        console.log(h1.innerHTML);
+
+        expect(h1.innerHTML).toBe(title);
+
+        // Que no sea estrictamente, sino que lo contenga
+        expect(h1.innerHTML).toContain(title);
+
     })
 }); 
