bf51d7ed016f8e26de41c9648f68df1abc505dc1
Pedro Araujo <araujopedrop@gmail.com>
2025-03-19 01:00:32 -0300

    GifExpertApp - Primer Hook

--- 04-gif-expert-app/src/GifExpertApp.jsx
+++ 04-gif-expert-app/src/GifExpertApp.jsx
@@ -1,9 +1,26 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 export const GifExpertApp = () => {
+
+
+    const [categories, setCategories] = useState([ 'One Punch Man', 'Dragon Ball' ]); 
+    console.log(categories);
+
     return (
     <>
+        {/* Titulo */}
         <h1>GifExpertApp</h1>
+
+        {/* Input */}
+        {/* Listado de Gifs */}
+        <ol>
+            { categories.map(category => {
+                return <li key={category}> {category} </li>
+            }) }
+            <li>ABC</li>
+        </ol>
+            {/* Gif Item */}
+
     </>
     )
 }
