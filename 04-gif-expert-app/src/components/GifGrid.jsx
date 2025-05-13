1cbb03a31d92eb952f72e0722395f1f482c71258
Pedro Araujo <araujopedrop@gmail.com>
2025-05-13 17:37:59 -0300

    Unidad 87

--- 04-gif-expert-app/src/components/GifGrid.jsx
+++ 04-gif-expert-app/src/components/GifGrid.jsx
@@ -1,21 +1,61 @@
-import { useEffect } from "react";
+import { useState ,useEffect } from "react";
+
+
 import { getGifs } from "../helpers/getGifs";
+import { GifItem } from "./GifItem";
 
 
 export const GifGrid = ({ category }) => {
 
-    useEffect( () => {getGifs()}, [] ); 
+    const [images, setImages] = useState([]);
 
+    //useEffect( () => {getGifs()}, [] ); // Si dejo vacio el array de dependencias, el useEffect solo se disparara la primera vez que se crea y se construye el componente
+
+    // No puedo pasar al useEffect un promesa, tiene que se una funcion
+    // Manera 1:
+    // useEffect( () => {getGifs(category).then( newImages => setImages(newImages))} );
+    // Manera 2: Crearme una funcion y esa pasarla al useEffect
+
+    const getImages = async() => {
+        const newImages = await getGifs(category);
+        setImages(newImages);
+    }
+    useEffect( () => {getImages()}, [] );
+
+    /*
     return (
-    <>
-        <h3>{ category }</h3>
+        <> 
+            <h3>{ category }</h3>
+    
+            <div className="card-grid">
+            { 
+                images.map((image) => (
+                    <GifItem key={image.id} title={image.title} url={image.url}/>
+                ))
+            }
+            </div>
+        </>
+        )
+    */
 
-        <ol>
-            <li>titulo</li>
-            <li>titulo</li>
-            <li>titulo</li>
-            <li>titulo</li>
-        </ol>
-    </>
-    )
+    // Usando operador spreed, para 'espacir' el image. 
+    // Esto sirve para que cada una de las propiedades de image las reciba como properties en el componente GifItem
+    return (
+        <> 
+            <h3>{ category }</h3>
+    
+            <div className="card-grid">
+            { 
+                images.map((image) => (
+                    <GifItem key={image.id} {...image}/>
+                ))
+            }
+            </div>
+        </>
+        )
+
+
+
 }
+
+
--- 04-gif-expert-app/src/helpers/getGifs.js
+++ 04-gif-expert-app/src/helpers/getGifs.js
@@ -1,21 +1,18 @@
 export const getGifs = async(category) => {
 
-    const url = `https://api.giphy.com/v1/gifs/search?api_key=evMA9XB2l14Qn0Cjmzlo9wHnIdLoTSrn&q=${category}&limit=20`;
+    const url = `https://api.giphy.com/v1/gifs/search?api_key=evMA9XB2l14Qn0Cjmzlo9wHnIdLoTSrn&q=${category}&limit=10`;
     const resp = await fetch( url );
-    const { data } = await resp.json();
-
-    //console.log(data);
+    const { data = []  } = await resp.json();
 
     const gifs = data.map ( img => ({
             id: img.id,
             title: img.title,
             url: img.url
-
         })
-  
     );
 
     console.log(gifs);
     return gifs;
 
 }
+ 
